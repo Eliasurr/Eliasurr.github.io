8fe17d5f72a2d4408322c8527e903df0fcbead65
Ulises Rando <ulisees.rando@gmail.com>
2022-04-20 12:29:14 -0300

    clase14

---
+++ clase-12/js/variables.js
@@ -0,0 +1,24 @@
+/*Variables*/
+
+let edad = 10; //type number
+console.log(edad, typeof edad);
+// Se puede cambiar el valor de la edad
+edad = 20;
+
+//variable NO modificable
+const dni = 39835678;
+
+let nombre = 'ulises';
+let apellido = 'rando'; //type string
+
+console.log(nombre, apellido, typeof nombre, typeof apellido);
+
+//type boolean
+const flag = true;
+console.log(flag, typeof flag);
+
+//arrays
+
+const nombres = []; //array vacio
+
+const materias = ['matematica', 'lengua', 'ingles', 'fisica'];
